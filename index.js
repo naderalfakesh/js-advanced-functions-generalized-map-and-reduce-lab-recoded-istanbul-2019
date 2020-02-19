6bbcfb8f0d8aa2a85415a0bdd5b7782f14951b25
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 22:29:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your functions here
 function map(arr,f){
   const newArr = [...arr];
-  for(let i=0 ; i > newArr.length ; i++ ){
+  for(let i=0 ; i < newArr.length ; i++ ){
     newArr[i] = f(newArr[i]);
   }
   return newArr;
